d80a89c60cc9b367677cf7c9a11010a43774c2d7
Dennis Cai <dennis@thecodinganalyst.com>
2021-03-25 16:32:53 +0800

    first e2e test with protractor

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,16 +1,10 @@
 import { browser, logging } from 'protractor';
-import { AppPage } from './app.po';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
-
-  it('should display welcome message', async () => {
-    await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('elixir app is running!');
+  it('should have title as Elixir', async () => {
+    await browser.get(browser.baseUrl);
+    expect(await browser.getTitle()).toEqual('Elixir');
   });
 
   afterEach(async () => {
